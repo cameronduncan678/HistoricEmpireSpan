b79ffd910e24db4d324837f19c3b6d68c52508bf
Cameron Duncan <cameronduncan678@gmail.com>
2020-07-31 09:50:52 +0100

    new data file

--- js/data.js
+++ js/data.js
@@ -1,76 +1,61 @@
-//Test data
-const dummyData = [
-    {empireName: 'Roman', span: '3.71'},
-    {empireName: 'British', span: '26.35'},
-    {empireName: 'Mongol', span: '17.81'},
-]
-
 //Function for dynamic SVG canvas height
-function svgCanvasHeight(dataArray){
-    return dataArray.length * 65 + 20;
-};
+function svgCanvasHeight(dataArray) {
+  return dataArray.length * 65 + 20;
+}
 
-d3.csv('../data/historicalEmpireData_Format.csv').then(data =>{
+d3.csv('../data/empireData.csv').then((data) => {
+  data.forEach((el) => {
+    el.MaximumlandareaPerc = Number(el.MaximumlandareaPerc);
+  });
 
-    data.forEach(el => {
-        el.MaximumlandareaPerc = Number(el.MaximumlandareaPerc);
-    });
+  console.log(data);
+  const canvasWidth = 585;
+  const canvasHeight = svgCanvasHeight(data);
 
-    console.log(data);
-    const canvasWidth = 585;
-    const canvasHeight = svgCanvasHeight(data);
-
-    const svg = d3.select('#data-canvas')
+  const svg = d3
+    .select('#data-canvas')
     .attr('width', canvasWidth)
     .attr('height', canvasHeight);
 
-    //Scales
-    var x_scale = d3.scaleLinear()
-    .domain([0, 50])
-    .range([0, 585]);
+  //Scales
+  var x_scale = d3.scaleLinear().domain([0, 50]).range([0, 585]);
 
+  const greyRects = svg.selectAll('rect.background').data(data);
 
-    const greyRects = svg.selectAll('rect.background').data(data);
-
-    greyRects.enter()
+  greyRects
+    .enter()
     .append('rect')
     .attr('width', 550)
     .attr('height', 40)
-    .attr('y', (d,i) => {
-        return i * 65
+    .attr('y', (d, i) => {
+      return i * 65;
     })
     .attr('fill', 'grey')
     .classed('background', true);
 
-    const redRects = svg.selectAll('rect.colorBar').data(data);
+  const redRects = svg.selectAll('rect.colorBar').data(data);
 
-    redRects.enter()
+  redRects
+    .enter()
     .append('rect')
-    .attr('width', (d) => x_scale(d.MaximumlandareaPerc))
+    .attr('width', (d) => x_scale(d.span))
     .attr('height', 40)
-    .attr('y', (d,i) => {
-        return i * 65
+    .attr('y', (d, i) => {
+      return i * 65;
     })
     .attr('fill', 'red')
     .classed('colorBar', true);
 
-    const dataNames = svg.selectAll('text').data(data);
+  const dataNames = svg.selectAll('text').data(data);
 
-        dataNames.enter()
-        .append('text')
-        .attr('text-anchor', 'end')
-        .attr('x', 550)
-        .attr('y', (d,i) =>{
-            return (i + 0.4) * 65
-        })
-        .text((d) => d.Empire)
-        .attr('fill', 'white');
+  dataNames
+    .enter()
+    .append('text')
+    .attr('text-anchor', 'end')
+    .attr('x', 550)
+    .attr('y', (d, i) => {
+      return (i + 0.4) * 65;
+    })
+    .text((d) => d.name)
+    .attr('fill', 'white');
 });
-
-
-
-
-
-
-
-    
